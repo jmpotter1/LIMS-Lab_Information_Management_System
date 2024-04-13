ac0028d0764270ec95672df1c11e5db629af8772
jmpotter1 <103273867+jmpotter1@users.noreply.github.com>
2024-04-12 19:45:36 -0500

    Initial Commit

---
+++ src/behavioral_analysis/school_shooter_traits.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int school_shooter_traits() {
+    printf("Personality Traits and Behavior:");
+    
+    char leakageClues[] = {"subtle threats", "boasts", "innuendos", "predictions", "ultimatums"};
+    char leakageClueForms[] = {"spoken", "conveyed in stories", "diary entries", "essays", "poems", "letters", "songs", "drawings", "doodles", "tattoos", "videos"};
+    char leakageExamples[] = "a student who shows a recurring preoccupation with themes of violence, hopelessness, dispair, hatred, isolation, loneliness, nihilism, or an 'end of the world' philosphy. 
+                              Those Themes may be expressed in conversation, or in jokes";
+
+
+    printf("%s", leakageClues[0]);
+    printf("%s", leakageClues[1]);
+    printf("%s", leakageClues[2]);
+    printf("%s", leakageClues[3]);
+    printf("%s", leakageClues[4]);
+
+    printf("%s", leakageClueForms[0]);
+    printf("%s", leakageClueForms[1]);
+    printf("%s", leakageClueForms[2]);
+    printf("%s", leakageClueForms[3]);
+    printf("%s", leakageClueForms[4]);
+    printf("%s", leakageClueForms[5]);
+    printf("%s", leakageClueForms[6]);
+    printf("%s", leakageClueForms[7]);
+    printf("%s", leakageClueForms[8]);
+    printf("%s", leakageClueForms[9]);
+    printf("%s", leakageClueForms[10]);
+
+    printf("Leakage can also be a cry for help, a sign of inner conflict, or boasts that may look empty but actually express a serious threat.");
+    printf("Leakage is considered to be one of the most important clues that may precede an adolescent's violent acts.");
+
+}
---
+++ src/fmp_locate.py
@@ -0,0 +1,21 @@
+import 'google_search.py'
+import 'facebook_seach.py'
+import 'instagram_search.py'
+import 'x_search.py'
+
+
+# FMP Search Parameters
+
+fmp_legal_name = input ("Enter FMPs Legal Name:")
+fmp_date_of_birth = input ("Enter FMPs Date of Birth:")
+fmp_place_of_birth = input ("Enter FMPs Place of Birth:")
+fmp_hair_color = input ("Enter FMPs Hair Color:")
+fmp_eye_color = input ("Enter FMPs Eye Color:")
+fmp_height = input ("Enter FMPs Height:")
+fmp_weight = input ("Enter FMPs Weight:")
+fmp_sex = input ("Enter FMPs Birth Sex:")
+fmp_nationality = input ("Enter FMPs Nationality:")
+fmp_race = input ("Enter FMPs Race:")
+fmp_languages_spoken = input ("Enter FMPs Languages Spoken:")
+fmp_other_known_information = input ("Enter Other Known Information on the FMP:")
+
