22752cd325be449818d90ac20414a748311eef5c
jmpotter1 <103273867+jmpotter1@users.noreply.github.com>
2024-04-15 13:19:42 -0500

    Create fmp_located.py specific location elevation values

---
+++ src/fmp_located.py
@@ -0,0 +1,41 @@
+import 'fmp_locate'
+
+if elevation == 7492:
+	print("Nowshāk Afghanistan")
+elif elevation == 258:
+	print("Amu Darya Afghanistan")
+elif elevation == 922:
+	print("Herāt Afghanistan")
+elif elevation == 1066:
+	print("Shīndand Afghanistan")
+elif elevation == 476:
+	print("Zaranj Afghanistan")
+elif elevation == 1010:
+	print("Kandahār Afghanistan")
+elif elevation == 2219:
+	print("Ghaznī Afghanistan")
+elif elevation == 2308:
+	print("Garzēz Afghanistan")
+elif elevation == 575:
+	print("Jalālābād Afghanistan")
+elif elevation == 1791:
+	print("Kabul Afghanistan")
+elif elevation == 1488:
+	print("Bagrām Afghanistan")
+elif elevation == 351:
+	print("Kunduz Afghanistan")
+elif elevation == 330:
+	print("Qizil Qal'ah (Shir Khan) Afghanistan")
+elif elevation == 300:
+	print("Haīratān Afghanistan")
+elif elevation == 357:
+	print("Mazār-e Sharīf Afghanistan")
+elif elevation == 926:
+	print("Maīmanah Afghanistan")
+elif elevation == 678:
+	print("Tōraghūndī Afghanistan")
+elif elevation == 773:
+	print("Lashkar Gāh Afghanistan")
+else elevation:
+	print("Location Not Found")
+	
