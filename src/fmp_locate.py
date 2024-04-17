316c4663f6de4da236835727b120fa4bf9aae3be
jmpotter1 <103273867+jmpotter1@users.noreply.github.com>
2024-04-17 13:54:43 -0500

    added additional import links

--- src/fmp_locate.py
+++ src/fmp_locate.py
@@ -1,7 +1,19 @@
-import 'google_search.py'
-import 'facebook_seach.py'
-import 'instagram_search.py'
-import 'x_search.py'
+import 'google_search'
+import 'facebook_search'
+import 'instagram_search'
+import 'x_search'
+import 'twitter_search'
+import 'truth_media_search'
+import 'fbi_wanted_search'
+import 'dea_wanted_search'
+import 'rewards_for_justice_search'
+import 'dot-oig_wanted_search'
+import 'us_department_of_state_wanted_search'
+import 'hhs-oig_wanted_search'
+import 'fda_wanted_search'
+import 'ice_wanted_search'
+import 'secret_search_wanted_search'
+import 'us_marshals_wanted_search'
 
 
 # FMP Search Parameters
@@ -18,4 +30,3 @@ fmp_nationality = input ("Enter FMPs Nationality:")
 fmp_race = input ("Enter FMPs Race:")
 fmp_languages_spoken = input ("Enter FMPs Languages Spoken:")
 fmp_other_known_information = input ("Enter Other Known Information on the FMP:")
-
